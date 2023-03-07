9d3cb17319b54e79e00a100ad34f49e4eebfe3f5
Mohammedyasin0504 <124041993+Mohammedyasin0504@users.noreply.github.com>
2023-03-07 23:28:00 +0530

    Add files via upload

---
+++ word.java
@@ -0,0 +1,22 @@
+public class word
+{
+public static viod main (String args[])
+String s = "narayana swamy resinged the cheif minister from pondicherry";
+int count = 0;
+for(int i = 0; i<s.length();i=i+1);
+{
+if(s.charAt(i)== ' ')
+{
+count = count +1 ;
+}
+}
+System.out.println(count);
+
+
+
+
+
+
+
+
+}
